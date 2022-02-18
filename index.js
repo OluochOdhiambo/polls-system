754e52bfb9d2283092a819c60969c11ee95550c3
unknown <oluochodhiambo11@gmail.com>
2022-02-18 10:07:58 +0300

    deploy

--- index.js
+++ index.js
@@ -23,6 +23,13 @@ app.use("/api/users", userRoute);
 app.use("/api/questions", questionRoute);
 app.use("/api/respondents", respondentRoute);
 
+// DEPLOYMENT
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
